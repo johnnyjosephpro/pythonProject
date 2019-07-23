f6ce151ed671b109310b473397ad6cbf02603ee7
Johnny Joseph <johnnyjosephpro@gmail.com>
2019-07-23 18:11:07 -0400

    Add too files

---
+++ Excercice.py
@@ -0,0 +1,15 @@
+#Exercice PYTHON 1
+print ("Welcome to Python Test 1")
+
+#Declaration variable
+r,pi= 12, 3.14159
+s=pi*r**2
+#Print la valeur de S
+print(s)
+
+#Les types des valeurs
+print(type(r),type(pi),type(s))
+
+# Exercice PYTHON 2
+
+
---
+++ While.py
@@ -0,0 +1,18 @@
+Python 3.8.0a4 (v3.8.0a4:c1004b8546, May  7 2019, 13:53:12) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> a=0
+>>> while (a<7):
+	a=a+1
+	print(a)
+
+	
+1
+2
+3
+4
+5
+6
+7
+>>> 
